13848d857811709c0da5d4f80cb5bbd808aebc40
Nguyễn Duy Luận <duyluan0216@gmail.com>
2022-10-26 17:06:38 +0700

    First commit máy tính

---
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.calculatorapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private var tvInput: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.btnSeven)
+        tvInput = findViewById(R.id.tvInput)
+    }
+
+    fun onDigit(view: View) {
+        tvInput?.append((view as Button).text)
+    }
+
+    fun onClear(view: View) {
+        tvInput?.text = ""
+    }
+}
